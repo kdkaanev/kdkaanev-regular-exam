12feb93386911bc70528fd2c7a6475b6631c9032
kdkaanev <kdkaanev@github.com>
2024-06-30 14:14:12 +0300

    change port

--- tests/ui/about.test.js
+++ tests/ui/about.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
-test('Check about page', async ({ page }) => {
-  await page.goto('http://localhost:8080/about');
+test.only('Check about page', async ({ page }) => {
+  await page.goto('http://localhost:8081/about');
   const heading = await page.$('h3');
   const text = await heading.textContent();
   expect(text).toBe('About us');
--- tests/ui/add-book.test.js
+++ tests/ui/add-book.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check add book page', async ({ page }) => {
-    await page.goto('http://localhost:8080/add-a-book');
+    await page.goto('http://localhost:8081/add-a-book');
     const form = await page.$('list');
     expect(form).toBeTruthy();
   });
